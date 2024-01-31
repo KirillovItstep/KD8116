51df6662f4784ad0659b456131aafeccc286e5ad
kirillovItStep <kirillov.malp@gmail.com>
2024-01-31 11:02:42 +0300

    KD

--- git.bat
+++ git.bat
@@ -1,9 +1,14 @@
-git config --global i18n.commitencoding utf-8
-git config --global i18n.logoutputencoding utf-8
+
 cd "D://KD8116/git"
 git init
-git add --renormalize .
+git add .
 git commit -m "KD"
 git remote remove origin
 git remote add origin https://KirillovItstep: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@github.com/KirillovItstep/KD8116.git
 git push --set-upstream origin master
+
+
+Get-ChildItem *.m -Recurse | ForEach-Object {
+$content = $_ | Get-Content
+
+Set-Content -PassThru $_.Fullname $content -Encoding UTF8 -Force}
